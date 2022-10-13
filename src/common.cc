e3b6b32806b73827c87e05d82f2bddf4260453d7
cyckun <cyckun@aliyun.com>
2022-10-13 20:14:54 +0800

    fix cpplint err

--- src/common.cc
+++ src/common.cc
@@ -1,4 +1,20 @@
-
+/*
+ *
+ * Copyright 2022 crypto-admin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 #include <random>
 #include <string>
 #include <sstream>
@@ -11,8 +27,9 @@ namespace PSI
 {
 
     // p = 2^256-2^224-2^96+2^64-1
-small P[DIG_LEN] =
-{ 0xFFFFFFFF,0xFFFFFFFF,0x00000000,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFE };
+small P[DIG_LEN] = {
+    0xFFFFFFFF, 0xFFFFFFFF, 0x00000000, 0xFFFFFFFF,
+    0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFE };
 
 int GetRandom(int length, unsigned char * dst) {
   std::random_device rd;
@@ -91,4 +108,4 @@ affpoint PointNeg(affpoint src) {
 }
 
 
-} // namespace PSI
+}  // namespace PSI
